bfa6c46d8c2f2464bc7a025a5dc6dae4c61bce43
bijuVB <72310879+bijuVB@users.noreply.github.com>
2024-08-29 08:11:50 +0530

    New line committed

--- MyMavenProject1/src/main/java/mavenpack1/HelloWorldMaven.java
+++ MyMavenProject1/src/main/java/mavenpack1/HelloWorldMaven.java
@@ -5,6 +5,7 @@ public class HelloWorldMaven {
 	public static void main(String[] args) {
 		System.out.println("Hello World Maven");
 		System.out.println("Hi Java");
+		System.out.println("hello");
 
 	}
 
